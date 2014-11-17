9708daa4af6689fe97331a7034fa0ca8d209c0d2
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-11-17 19:03:13 +0300

    Fix last 'fix'

--- verification/multi_score.py
+++ verification/multi_score.py
@@ -30,7 +30,6 @@ class CheckiORefereeMultiScore(CheckiORefereeMulti):
         if not is_win_result:
             self.test_current_step()
         else:
-            self.total_score += self.referee_data["total_score"]
             if self.next_env():
                 self.restart_env()
             else:
